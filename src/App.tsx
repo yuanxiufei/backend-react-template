d4916c3b6cf430e1a81d967b026a5bd265a974e3
ReginYuan <2496943860@qq.com>
2025-04-01 00:40:22 +0800

    feat: 整理项目

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { RouterProvider } from 'react-router-dom'
+import { ConfigProvider, App as AntdApp, theme } from 'antd'
+import router from './router'
+import AntdGlobal from './utils/AntdGlobal'
+import './App.less'
+import './styles/theme.less'
+import { useStore } from './store'
+function App() {
+  const isDark = useStore(state => state.isDark)
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ed6c00'
+        },
+        algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm
+      }}
+    >
+      <AntdApp>
+        <AntdGlobal />
+        <RouterProvider router={router} />
+      </AntdApp>
+    </ConfigProvider>
+  )
+}
+
+export default App
